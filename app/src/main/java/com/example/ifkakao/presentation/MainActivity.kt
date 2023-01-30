9a9222350631561bc11e2f98be35ec1c996ea3ca
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-01-30 13:13:06 +0900

    KAKAO-9 chore: CI 테스트

--- app/src/main/java/com/example/ifkakao/presentation/MainActivity.kt
+++ app/src/main/java/com/example/ifkakao/presentation/MainActivity.kt
@@ -7,6 +7,7 @@ import com.example.ifkakao.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
     }
 }

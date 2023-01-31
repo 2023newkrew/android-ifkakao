8aee1b5a217c68e80acefc641f6640c99658c45a
allen <32155219+s45000@users.noreply.github.com>
2023-02-01 02:21:14 +0900

    feat: MainVideoFragment 비디오 재생 ( VideoView 버전 )

--- app/src/main/java/com/example/ifkakao/presenter/fragment/MainVideoFragment.kt
+++ app/src/main/java/com/example/ifkakao/presenter/fragment/MainVideoFragment.kt
@@ -1,24 +1,43 @@
 package com.example.ifkakao.presenter.fragment
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import com.example.ifkakao.R
+import com.example.ifkakao.databinding.FragmentMainVideoBinding
 
 class MainVideoFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentMainVideoBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_video, container, false)
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentMainVideoBinding.inflate(inflater, container, false)
+
+        val VIDEO_PATH = "android.resource://" + "com.example.ifkakao" + "/" + R.raw.main_video
+        val uri: Uri = Uri.parse(VIDEO_PATH)
+
+        binding.mainVideoView.setVideoURI(uri)
+        binding.mainVideoView.start()
+        binding.mainVideoView.setOnPreparedListener {
+            it.isLooping = true
+            it.start()
+            binding.mainVideoView.layoutParams.height = WRAP_CONTENT
+        }
+
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }

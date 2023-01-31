9d6917b289bff68176aa0781315f39e3e752f934
allen <32155219+s45000@users.noreply.github.com>
2023-02-01 05:32:11 +0900

    feat: add MainVideoFragment feat: add HelloFragment feat: add ChoiceSessionTypeFragment feat: add ChoiceSessionTrackFragment

--- app/src/main/java/com/example/ifkakao/presenter/activity/MainActivity.kt
+++ app/src/main/java/com/example/ifkakao/presenter/activity/MainActivity.kt
@@ -9,6 +9,7 @@ import com.example.ifkakao.di.MyApplication
 import com.example.ifkakao.di.component.MainComponent
 import com.example.ifkakao.presenter.fragment.ChoiceSessionTrackFragment
 import com.example.ifkakao.presenter.fragment.ChoiceSessionTypeFragment
+import com.example.ifkakao.presenter.fragment.HelloFragment
 import com.example.ifkakao.presenter.fragment.MainVideoFragment
 import com.example.ifkakao.presenter.viewmodel.MainActivityViewModel
 import javax.inject.Inject
@@ -30,6 +31,7 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
                 add<MainVideoFragment>(R.id.main_video_fragment_container_view)
+                add<HelloFragment>(R.id.hello_fragment_container_view)
                 add<ChoiceSessionTypeFragment>(R.id.choice_session_type_fragment_container_view)
                 add<ChoiceSessionTrackFragment>(R.id.choice_session_track_fragment_container_view)
             }
--- app/src/main/java/com/example/ifkakao/presenter/fragment/MainVideoFragment.kt
+++ app/src/main/java/com/example/ifkakao/presenter/fragment/MainVideoFragment.kt
@@ -2,19 +2,16 @@ package com.example.ifkakao.presenter.fragment
 
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import androidx.fragment.app.Fragment
 import com.example.ifkakao.R
 import com.example.ifkakao.databinding.FragmentMainVideoBinding
 
 class MainVideoFragment : Fragment() {
-
     private var _binding: FragmentMainVideoBinding? = null
     private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,7 +19,7 @@ class MainVideoFragment : Fragment() {
     ): View {
         _binding = FragmentMainVideoBinding.inflate(inflater, container, false)
 
-        val VIDEO_PATH = "android.resource://" + "com.example.ifkakao" + "/" + R.raw.main_video
+        val VIDEO_PATH = "android.resource://" + (activity?.packageName ?: "com.example.ifkakao") + "/" + R.raw.main_video
         val uri: Uri = Uri.parse(VIDEO_PATH)
 
         binding.mainVideoView.setVideoURI(uri)
@@ -30,7 +27,7 @@ class MainVideoFragment : Fragment() {
         binding.mainVideoView.setOnPreparedListener {
             it.isLooping = true
             it.start()
-            binding.mainVideoView.layoutParams.height = WRAP_CONTENT
+            binding.mainVideoView.layoutParams.height = it.videoHeight
         }
 
         return binding.root

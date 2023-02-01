3f6bdf914fe3f3f1bda7ec3c9bc9a89003f454fc
allen <32155219+s45000@users.noreply.github.com>
2023-02-02 04:53:27 +0900

    feat: Activity에서 callBack으로 Fragment변경

--- app/src/main/java/com/example/ifkakao/presentation/main/activity/MainActivity.kt
+++ app/src/main/java/com/example/ifkakao/presentation/main/activity/MainActivity.kt
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.example.ifkakao.R
 import com.example.ifkakao.di.MyApplication
 import com.example.ifkakao.di.component.MainComponent
 import com.example.ifkakao.presentation.main.fragment.*
 import com.example.ifkakao.presentation.main.listener.MainActivityListener
 import com.example.ifkakao.presentation.main.viewmodel.MainActivityViewModel
+import com.example.ifkakao.presentation.session_list.SessionListFragment
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(), MainActivityListener {
@@ -34,7 +36,10 @@ class MainActivity : AppCompatActivity(), MainActivityListener {
     }
     override fun fragmentCallBack(code: MainActivityListener.Code) {
         when (code){
-            MainActivityListener.Code.GO_TO_SESSION_LIST -> println("allen : ActivityGo")
+            MainActivityListener.Code.GO_TO_SESSION_LIST -> supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                replace<SessionListFragment>(R.id.main_fragment_container_view)
+            }
         }
     }
 }

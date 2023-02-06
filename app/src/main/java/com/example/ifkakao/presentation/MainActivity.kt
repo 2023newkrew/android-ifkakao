0031ed584deb959e764b58457a69eefbbabee368
Ktaewon <dydalehdxo@naver.com>
2023-02-06 21:35:35 +0900

    comment: 주석 제거

--- app/src/main/java/com/example/ifkakao/presentation/MainActivity.kt
+++ app/src/main/java/com/example/ifkakao/presentation/MainActivity.kt
@@ -62,7 +62,6 @@ class MainActivity : AppCompatActivity() {
                     navController.navigate(R.id.list)
             } else if (id == R.id.CoC) {
                 it.isChecked = false
-                // 안전하게 바꾸기
                 val browserIntent: Intent = Intent(
                     Intent.ACTION_VIEW, Uri.parse(
                         "https://mk.kakaocdn.net/dn/if-kakao/2022/if_kakao_code_of_conduct_v1.1.pdf"
